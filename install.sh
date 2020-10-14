4be650b70720a0a7275ce329d34d78c05d74f408
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-10-14 15:37:53 +0000

    add main in install.sh, fix nix script links

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+# TODO replace this with hostname dirs
 SPECIAL="bash bash_hpc readme.md install.sh firefox"
 show_n_stow() { cmd="stow --verbose '$1'"; echo "$cmd"; eval "$cmd"; }
 
 # go to dotfiles dir
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# install shared dotfiles
-ls | while read d; do
-  [[ $SPECIAL =~ $d ]] || show_n_stow "$d"
-done
+main() {
+  # install shared dotfiles
+  ls | while read d; do
+    [[ $SPECIAL =~ $d ]] || show_n_stow "$d"
+  done
 
-# install per-host dotfiles
-[[ $(hostname) == agora ]] && show_n_stow firefox
-[[ $(hostname) =~ brc ]] && show_n_stow bash_hpc || show_n_stow bash
+  # install per-host dotfiles
+  [[ $(hostname) == agora ]] && show_n_stow firefox
+  [[ $(hostname) =~ brc ]] && show_n_stow bash_hpc || show_n_stow bash
+}
+
+main
